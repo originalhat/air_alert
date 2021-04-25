82e1d56081b6785c4434d815e58ec576a171a54e
Devin Brown <devin.g.brown@gmail.com>
2021-04-25 10:58:42 -0600

    storing subscriptions in DB

--- lib/air_alert_web/live/feed_live.ex
+++ lib/air_alert_web/live/feed_live.ex
@@ -1,6 +1,8 @@
 defmodule AirAlertWeb.FeedLive do
   use AirAlertWeb, :live_view
 
+  alias AirAlert.Subscription
+
   @impl true
   def mount(%{"name" => name, "url" => url}, _session, socket) do
     AirAlertWeb.Feed.start()
@@ -8,7 +10,14 @@ defmodule AirAlertWeb.FeedLive do
     %{"aqi" => aqi, "forecast" => %{"daily" => %{"pm25" => pm25_forecast}}} =
       AirAlertWeb.Feed.get!(url).body[:data]
 
-    {:ok, assign(socket, name: name, aqi: aqi, forecasts: pm25_forecast |> Enum.take(-3))}
+    {:ok,
+     assign(socket,
+       name: name,
+       aqi: aqi,
+       forecasts: pm25_forecast |> Enum.take(-3),
+       changeset: Subscription.changeset(%Subscription{}, %{}),
+       submit: false
+     )}
   end
 
   @impl true
@@ -41,29 +50,54 @@ defmodule AirAlertWeb.FeedLive do
 
       <p>Receive SMS alerts when the AQI exceeds safe levels for <strong><%= @name %></strong>.</p>
 
-      <form phx-submit="configure-alert" phx-change="validate-alert">
-        <label>AQI threshold<label>
-        <select name="aqi">
-          <option value="good">Good (0-50)</option>
-          <option value="moderate">Moderate (51-100)</option>
-          <option value="sensitive">Unhealthy for sensitive groups (101-150)</option>
-          <option value="unhealthy">Unhealthy (151-200)</option>
-          <option value="very_unhealthy">Very unhealthy (201-300)</option>
-          <option value="hazardous">Hazardous (301+)</option>
-        </select>
+      <%= if @submit do %>
+        <p><em>Thanks for signing up for notifications!<em></p>
+      <% else %>
+        <%= f = form_for @changeset, "#", [phx_change: :validate, phx_submit: :save] %>
+          <%= label f, :phone_number %>
+          <%= telephone_input f, :phone_number, placeholder: "Phone", autocomplete: "off", phx_debounce: "blur" %>
+          <%= error_tag f, :phone_number %>
 
-        <label>Phone number</label>
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
+          <%= label f, :threshold %>
+          <%= select f, :threshold, aqi_keys() %>
+          <%= error_tag f, :threshold %>
 
-        <button>Submit</button>
-      </form>
+          <%= submit "Save" %>
+        </form>
+      <% end %>
     </section>
     """
   end
 
+  def aqi_keys() do
+    [
+      "Good (0-50)": "good",
+      "Moderate (51-100)": "moderate",
+      "Unhealthy for sensitive groups (101-150)": "sensitive",
+      "Unhealthy (151-200)": "unhealthy",
+      "Very unhealthy (201-300)": "very_unhealthy",
+      "Hazardous (301+)": "hazardous"
+    ]
+  end
+
   @impl true
-  def handle_event("validate-alert", params, socket) do
-    IO.inspect(params)
-    {:noreply, socket}
+  def handle_event("validate", %{"subscription" => subscription_params}, socket) do
+    changeset =
+      %Subscription{}
+      |> Subscription.changeset(subscription_params)
+      |> Map.put(:action, :insert)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  @impl true
+  def handle_event("save", %{"subscription" => subscription_params}, socket) do
+    case Subscription.create_subscription(subscription_params) do
+      {:ok, _subscription} ->
+        {:noreply, assign(socket, submit: true)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
